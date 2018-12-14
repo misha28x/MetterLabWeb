4fe67b1e57c355ddfcc66c958a948649778c5ce2
Blind Kai <blindkai1426@gmail.com>
2018-12-14 20:17:01 +0200

    protocols n tests to objects

--- backend/routes/uploads.js
+++ backend/routes/uploads.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const format = require('string-format-js');
 
- const router = express.Router();
+const router = express.Router();
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -20,8 +20,6 @@ connection.connect(err => {
 });
 
 router.post('', (req, res, next) => {
-  // Add data to database
-
   let varPart = "INSERT INTO `protocols`(`Номер_протоколу`, `Дата_та_час`, `Номер_установки`, `Системний_номер_установки`, `Номер_лічильника`, `Тип_лічильника`, `Призначення_лічильника`, `Температура`, `Рік_випуску`, `Накопичений_обєм`, `Широта`, `Довгота`, `Статус_витрати`, `Результат_тесту`, `Дата_підпису_протоколу`, `ПІБ_особи_підписувача`, `Статус`) ";
   let varData = "VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');";
   let formatedData = varData.format(req.body.bbiFileName, req.body.date, req.body.deviceNumber, null, req.body.counterNumber, req.body.type, null, req.body.temperature, req.body.productionYear, req.body.capacity, null, null, req.body.status, req.body.result, null, null, req.body.protocolStatus);
@@ -29,12 +27,11 @@ router.post('', (req, res, next) => {
   connection.query(varPart + formatedData);
 
   req.body.tests.forEach(test => {
-    varPart = "INSERT INTO `tests`(`Номер_протоколу`, `Назва_тесту`, `Задана_витрата`, `Обєм_еталону`, `Початкове_значення`, `Кінцеве_значення`, `Обєм_за_лічильником`, `Тривалість_тесту`, `Фактична_витрата`, `Статус_витрати`, `Фактична_похибка`, `Результат_тесту`) ";
-    varData = "VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');";
-    formatedData = varData.format(test.bbiFileName, test.name, test.installedExes, test.etalonCapacity, test.initValue, test.finalValue, test.counterCapacity, test.testDuration, test.mediumExes, test.isInZone, test.calculatedFault, test.result);
+    varPart = "INSERT INTO `tests`(`Номер_протоколу`, `Назва_тесту`, `Задана_витрата`, `Обєм_еталону`, `Початкове_значення`, `Кінцеве_значення`, `Обєм_за_лічильником`, `Тривалість_тесту`, `Фактична_витрата`, `Статус_витрати`, `Допустима_похибка`, `Фактична_похибка`, `Результат_тесту`) ";
+    varData = "VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');";
+    formatedData = varData.format(test.bbiFileName, test.name, test.installedExes, test.etalonCapacity, test.initValue, test.finalValue, test.counterCapacity, test.testDuration, test.mediumExes, test.isInZone, test.assumedFault, test.calculatedFault, test.result);
 
     connection.query(varPart + formatedData);
-
   });
 
   console.log('uploaded');
@@ -43,17 +40,83 @@ router.post('', (req, res, next) => {
 });
 
 router.get('', (req, res, next) => {
-	// connection.query("SELECT * FROM `protocols`", function (err, result) {
-	//   // Викидає помилку, якщо при виконанні щось йде не так
-	//   if (err) throw err;
-	//   // Поле виведення
-	//   console.log(result);
-	// });
 
-  // res.json(JSON.stringify(connection.query("SELECT * FROM `protocols`")));
-	// res.json; - відправка файлу на клієнт.
-	console.log("GET request");
-	res.status(201).json({message: 'success'});
+  var protocolArray = new Array();
+
+
+  connection.query('SELECT * from protocols', function (err, rows, fields) {
+    if (err) throw err;
+
+
+		  var testArray = new Array();
+
+    for (var i in rows) {
+      var rp = new Object();
+      rp.id = rows[i].id;
+      rp.protocolNumber = rows[i].Номер_протоколу;
+      rp.date = rows[i].Дата_та_час;
+      rp.deviceNumber = rows[i].Номер_установки;
+      rp.systemNumber = rows[i].Системний_номер_установки;
+      rp.counterNumber = rows[i].Номер_лічильника;
+      rp.type = rows[i].Тип_лічильника;
+      rp.counterPurpose = rows[i].Призначення_лічильника;
+      rp.temperature = rows[i].Температура;
+      rp.productionYear = rows[i].Рік_випуску;
+      rp.capacity = rows[i].Накопичений_обєм;
+      rp.width = rows[i].Широта;
+      rp.height = rows[i].Довгота;
+      rp.isInZone = rows[i].Статус_витрати;
+      rp.result = rows[i].Результат_тесту;
+      rp.signDate = rows[i].Дата_підпису_протоколу;
+      rp.signName = rows[i].ПІБ_особи_підписувача;
+      rp.status = rows[i].Статус;
+			rp.tests = [];
+			
+      var selection = "SELECT * FROM tests WHERE Номер_протоколу='" + rp.protocolNumber + "';";
+				
+      connection.query(selection, function (err, testRows, fields) {
+				if (err) throw err;
+	
+        for (var i in testRows) {
+          var rt = new Object();
+          rt.id = testRows[i].id;
+          rt.bbiFileName = testRows[i].Номер_протоколу;
+          rt.name = testRows[i].Назва_тесту;
+          rt.installedExes = testRows[i].Задана_витрата;
+          rt.etalonCapacity = testRows[i].Обєм_еталону;
+          rt.initValue = testRows[i].Початкове_значення;
+          rt.finalValue = testRows[i].Кінцеве_значення;
+          rt.counterCapacity = testRows[i].Обєм_за_лічильником;
+          rt.testDuration = testRows[i].Тривалість_тесту;
+          rt.mediumExes = testRows[i].Фактична_витрата;
+          rt.isInZone = testRows[i].Статус_витрати;
+          rt.assumedFault = testRows[i].Допустима_похибка;
+          rt.calculatedFault = testRows[i].Фактична_похибка;
+          rt.result = testRows[i].Результат_тесту;
+
+					testArray.push(rt);
+				//	console.log(testArray);
+        }
+					rp.tests = testArray;		
+      });
+      protocolArray.push(rp);
+    }
+
+		console.log(protocolArray[0]);
+	//	console.log('Another fucking test');
+	//	console.log(testArray);
+		
+		
+  });
+
+
+
+  // res.json; - відправка файлу на клієнт.
+  console.log("GET request");
+
+  res.status(201).json({
+    message: 'success'
+  });
 });
 
 module.exports = router;
