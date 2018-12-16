43156673d025870f54b0e84f1327459133fbfa97
Blind Kai <blindkai1426@gmail.com>
2018-12-16 23:11:35 +0200

    new-verifications.js stmt

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const uploadRoutes = require('./routes/uploads');
 const taskVerificationRoutes = require('./routes/task-verification');
+const newVerificationsRoutes = require('./routes/new-verifications');
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,5 +26,6 @@ app.use(bodyParser.urlencoded({
 
 app.use('/api/upload', uploadRoutes);
 app.use('/api/task-verification', taskVerificationRoutes);
+app.use('/api/new-verifications', newVerificationsRoutes);
 
 module.exports = app;
---
+++ backend/routes/new-verifications.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mysql = require('mysql');
+const format = require('string-format-js');
+
+const router = express.Router();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'water_counters',
+  user: 'root',
+  password: 'root',
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
+// 1) Запит для отримання усіх повірок get
+router.get('', (req, res, next) => {
+  con.query('SELECT * FROM new_verifications', (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.status(200).json(result);
+  });
+});
+// 2) Додавання нової повірки post
+router.post('', (req, res, next) => {
+  let varData = (" VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');");
+  let formatedData = varData.format( /** змінні для запиту */ );
+  let varResult = ("INSERT INTO `new_verifications`(`Номер_заявки`, `Дата_надходження`, `Клієнт`, `Надавач_послуг`," +
+    " `Район`, `Вулиця`, `Будинок`, `Квартира`, `Бажана_дата_повірки`, `Бажаний_час_повірки`, `Cправність_сантехніки`," +
+    " `Вода_відсутня_до`, `Наявність_пломби`, `Телефон`, `Примітка`)" + formatedData);
+
+  connection.query(varResult);
+});
+
+// 3) Редагуваня повірки put
+router.put('', (req, res, next) => {
+  let varData = "`Номер_заявки`='%s',`Дата_надходження`='%s',`Клієнт`='%s',`Надавач_послуг`='%s',`Район`='%s'," +
+    "`Вулиця`='%s',`Будинок`='%s',`Квартира`='%s',`Бажана_дата_повірки`='%s',`Бажаний_час_повірки`='%s', " +
+    " `Cправність_сантехніки`='%s',`Вода_відсутня_до`='%s',`Наявність_пломби`='%s',`Телефон`='%s',`Примітка`='%s'";
+  let formatedData = varData.format( /** змінні для запиту */ );
+  let varResult = "UPDATE new_verifications SET " + formatedData + " WHERE Номер_заявки = '" + /** змінна поля заявки */ +"';";;
+
+  connection.query(varResult);
+});
+
+// 4) Видалення повірки delete
+router.delete('', (req, res, next) => {
+	let varData = "DELETE FROM `new_verifications` WHERE `Номер_заявки`='%s';";
+	let formatedData = varData.format( /** змінні для запиту */ );
+	let varResult = formatedData;
+
+  connection.query(varResult);
+});
+
+module.exports = router;
