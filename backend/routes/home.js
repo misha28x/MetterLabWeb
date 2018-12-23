ae73b739759716dfa8b9ec7458f2f85ca50a95c2
Blind Kai <blindkai1426@gmail.com>
2018-12-23 13:46:17 +0200

    Added newlines and fixed requests

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -3,4 +3,21 @@ const mysql = require('mysql');
 
 const router = express.Router();
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'water_counters',
+  user: 'root',
+  password: '',
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
+
+
 module.exports = router;
