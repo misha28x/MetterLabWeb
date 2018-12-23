12467387ca32a8e5e9864329cf535ec674028a32
Blind Kai <blindkai1426@gmail.com>
2018-12-23 14:50:24 +0200

    some requests

---
+++ backend/routes/lab-requests.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const router = express.Router();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'water_counters',
+  user: 'root',
+  password: '',
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
+router.get('', (req, res, next) => {
+  connection.query('SELECT * FROM lab_requests', (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.status(200).json(result);
+  });
+});
+
+module.exports = router;
