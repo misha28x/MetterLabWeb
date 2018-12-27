275adeb465aabdd752d00af93d71ed9161364e6f
misha28x <misha1998x@gmail.com>
2018-12-27 11:29:44 +0200

    fixes

--- backend/routes/uploads.js
+++ backend/routes/uploads.js
@@ -454,8 +454,9 @@ router.post('', upload.single('file'), (req, res, next) => {
         }
       });
     });
-    getResultsFromDatabase(db);
   });
+
+  getResultsFromDatabase(db);
 
   res.status(201);
 })
