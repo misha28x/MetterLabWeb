159892ca278d1e51f7c05fd4080f7e5b81229036
Blind Kai <blindkai1426@gmail.com>
2019-01-31 23:47:27 +0200

    dates to xxxx-xx-xx (except files)

--- backend/routes/station-tasks.js
+++ backend/routes/station-tasks.js
@@ -33,7 +33,7 @@ router.get('/:id', (req, res, next) => {
 });
 
 // "SELECT * FROM `station_tasks` WHERE `task_status` != 'Виконано'"
-
+// TODO: додати перевірку за датою, щоб не виводило завдання, які ніяк не могли бути виконані
 router.get('/failed/:id', (req, res, next) => {
   console.log('resolved');
   connection.query("SELECT * FROM `station_tasks` WHERE `task_status` != 'Виконано'", (err, rows) => {
@@ -46,6 +46,7 @@ router.get('/failed/:id', (req, res, next) => {
     res.json(rows);
   });
 });
+
 
 router.get('/unresolved/:id', (req, res, next) => {
   console.log('resolved');
