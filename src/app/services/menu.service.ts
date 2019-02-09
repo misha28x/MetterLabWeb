fd6184c836ed2c1f42f5ffa3b0ad9e484c03fad5
misha28x <misha1998x@gmail.com>
2019-02-09 23:47:08 +0200

    added counters live update

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Socket } from 'ngx-socket-io';
 import { Store } from '@ngrx/store';
 
 import { IMenuItem } from '../interfaces/menu';
@@ -12,11 +13,22 @@ const menuUrl = 'http://localhost:3000/api/menu/';
 })
 export class MenuService {
   private menuSource$ = new BehaviorSubject<IMenuItem[]>([]);
-  private permission;
-  
-  constructor(private http: HttpClient, private store: Store<string>) { 
+  private menuUpdate = this.socket.fromEvent<any>('update');
+  private permission: any;
+
+  constructor(
+    private http: HttpClient,
+    private store: Store<string>,
+    private socket: Socket
+    ) { 
     this.store.select('permission').subscribe(permission => {
+      this.permission = permission;
       this.setMenu(permission);
+    });
+
+    this.menuUpdate.subscribe(() => {
+      console.log('update');
+      this.setMenu(this.permission);
     });
   }
 
