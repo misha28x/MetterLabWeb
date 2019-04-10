abd8835c1f2bb1b34da7b070e6a9f6c3968169d3
Blind Kai <43204960+BlindKai@users.noreply.github.com>
2019-04-10 12:23:02 +0300

    id -> name

--- backend/routes/service-providers.js
+++ backend/routes/service-providers.js
@@ -7,12 +7,21 @@ const router = express.Router();
 
 // створено для
 router.get('/provider/:id/:createFor', (req, res, next) => {
-	connection.query("SELECT * FROM `archive` WHERE `userId`='" + req.params.id + "' AND (`status`='Не визначено відповідальну особу' OR `status`='') AND `createFor` = '" + req.params.createFor + "';", (err, result) => {
-		if (err) {
-			res.json(err);
-		}
-		res.json(result);
-	});
+  connection.query("SELECT * FROM `archive` WHERE `userId`='" + req.params.id + "' AND (`status`='Не визначено відповідальну особу' OR `status`='') AND `createFor` = '" + req.params.createFor + "';", (err, result) => {
+    if (err) {
+      res.json(err);
+    }
+    res.json(result);
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  connection.query("SELECT `name` FROM `contractors` WHERE `id` = '" + req.params.id + "'", (err, result) => {
+    if (err) {
+      res.json(err);
+    }
+    res.json(result);
+  });
 });
 
 module.exports = router;
