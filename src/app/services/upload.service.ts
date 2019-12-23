511c7b8e9bdcabf0798baa291569db4aa4ae5957
misha28x <misha1198x@gmail.com>
2019-12-23 08:31:50 +0200

    added log

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -22,7 +22,7 @@ export class UploadService {
 
   public upload(files: Set<File>): void {
     const fileData: FormData = new FormData();
-
+    console.log(this.user);
     files.forEach(file => {
       fileData.append('file', file);
       this.http.post(url + this.user.serviceProvider, fileData).subscribe();
